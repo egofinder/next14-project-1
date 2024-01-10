593c47e517ffbc518ebef13df42479c263994531
Benjamin Jeon <baekhakjeon@gmail.com>
2024-01-10 14:18:26 -0800

    fetch posts update

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,19 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
-// import { getPosts } from "@/lib/data";
+import { getPosts } from "@/lib/data";
 
 // API Call
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {
-    cache: "no-store",
-    // next: { revalidate: 3600 },
-  });
+// const getData = async () => {
+//   const res = await fetch("http://localhost:3000/api/blog", {
+//     cache: "no-store",
+//     // next: { revalidate: 3600 },
+//   });
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-  return res.json();
-};
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
+//   return res.json();
+// };
 
 export const metadata = {
   title: "Blog",
@@ -21,9 +21,9 @@ export const metadata = {
 };
 
 const BlogPage = async () => {
-  const posts = await getData();
+  // const posts = await getData();
 
-  // const posts = await getPosts();
+  const posts = await getPosts();
   return (
     <div className={styles.container}>
       {posts.map((post) => (
